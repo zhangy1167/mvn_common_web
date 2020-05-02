14c26065cb5d60352e0bfcf366d5dae984ab52ab
zhangy1167 <1301059287@qq.com>
2020-05-02 20:21:52 +0800

    创建代码提交到github

---
+++ src/main/java/com/zy/Main.java
@@ -0,0 +1,18 @@
+package com.zy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author :zy
+ * @创建日期: 2020年5月2日 下午8:09:52
+ */
+public class Main {
+
+	private static Logger log = LoggerFactory.getLogger(Main.class);
+	
+	public static void main(String[] args) {
+		log.info("测试日志正常打印");
+	}
+
+}
